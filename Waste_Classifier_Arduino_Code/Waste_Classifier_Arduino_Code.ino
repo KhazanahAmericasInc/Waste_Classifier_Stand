db69365ec79d641108c810102f94553ee7cb2bd4
CraigCrundwell <1crundwellcr@gmail.com>
2019-11-04 13:45:42 -0800

    Added more comments to code

--- Waste_Classifier_Arduino_Code/Waste_Classifier_Arduino_Code.ino
+++ Waste_Classifier_Arduino_Code/Waste_Classifier_Arduino_Code.ino
@@ -1,15 +1,31 @@
+//Craig Crundwell
+//Oct 30 2019
+//This program is run on the arduino Nano inside the waste classifier
+//The program reads a touch sensor and controls the LED lights
+
+//Library for capacitive sensing
 #include <CapacitiveSensor.h>
+
+//Creates sensor object attached to pins 2 and 3
 CapacitiveSensor Sensor = CapacitiveSensor(2,3);
 
 
-long val;
+long val; //used to store sensor reading 
+
 #define led 4
+
+
+//Adjust these value below to change touch sensitivity
+
+//*************************************
 #define threshold 15000
 #define hysteresis 5000
+//*************************************
+
 
 unsigned long delayStart = 0; // the time the delay started
-bool lightOn = false; // true if still waiting for delay to finish
-bool above = true; //
+bool lightOn = false; // true if light is on
+bool above = true; //tracks hysterisis of sensor to avoid flashing
 
 void setup() {
   Serial.begin(9600);
@@ -18,7 +34,7 @@ void setup() {
 }
 
 void loop() {
-  val = Sensor.capacitiveSensor(30);
+  val = Sensor.capacitiveSensor(30); //Gets reading from sensor
   Serial.println(val);
 
   //Double checks value if reading is above threshhold 
@@ -27,18 +43,20 @@ void loop() {
     val = Sensor.capacitiveSensor(30);
   }
 
+  //If value was above threshold and has now dropped below, set above to false so light can be touched again
   if (above && val < (threshold-hysteresis))
   {
     above = false;
   }
 
+  //Toggle light if touched
   else if (!above && val >= threshold)
   {
     if (lightOn == false)
     {
       digitalWrite(led, HIGH);
       lightOn = true;
-      delayStart = millis();
+      delayStart = millis();// Start tracking time for sleep timer
     }
   
     else
@@ -49,13 +67,12 @@ void loop() {
     above = true;
   }
 
+
+  //Turn off light after 5 minutes
   if (lightOn && ((millis() - delayStart) >= 300000))
   {
     lightOn = false;
     digitalWrite(led, LOW);
     
   }
-
-  
-
 }
